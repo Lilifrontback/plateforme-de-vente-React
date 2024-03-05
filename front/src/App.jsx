7d58e09eed412e28866f9f431c830014f9f8da87
PromiseHerb <146881805+PromiseHerb@users.noreply.github.com>
2024-03-05 16:46:06 +0100

    ajout barre de recherche

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SearchBar from './composants/SearchBar'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <div> <SearchBar/></div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +32,11 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
+    
   )
+
+
+
 }
 
 export default App
---
+++ front/src/composants/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <form>
+    <input type="text" placeholder="Search..." />
+  </form>
+  );
+};
+
+export default SearchBar;
