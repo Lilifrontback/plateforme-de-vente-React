f3fc6f211f123fe402cb1d2bdcd5169b39561c43
violaine-drt <viodarrent@gmail.com>
2024-03-06 16:36:28 +0100

    connexion server / database. First rooting to GET all Meubles

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const sql = require('mssql'); // MS Sql Server client
+const mysql = require('mysql') //
+const database = mysql.createConnection({
+  host: 'mysql-seguin.alwaysdata.net',
+  user: 'seguin_admin',
+  password: 'Admin1234!',
+  database: 'seguin_chaisemusicale',
+})
 
-// Connection string parameters.
-const sqlConfig = {
-    user: 'seguin_admin',
-    password: 'Admin1234!',
-    server: 'mysql-seguin.alwaysdata.net',
-    database: 'seguin_chaisemusicale'
-}
+database.connect();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
@@ -20,15 +20,13 @@ app.get('/', (req, res) => {
 })
 
 app.get('/meubles', function (req, res) {
-  sql.connect(sqlConfig, function() {
-      const request = new sql.Request();
-      console.log(request)
-      request.query('select * from Meubles', function(err, recordset) {
-          if(err) console.log(err);
-          res.end(JSON.stringify(recordset)); // Result in JSON format
-          console.log("je teste des trucs");
-          console.log(recordset['Meubles']);
-      });
-  });
+database.query("SELECT * FROM Meubles", (err, rows, fields) => {
+  if (err) {
+    console.log("erreur dans la requête",err)
+    res.status(500).send('erreur interne du serveur');
+    return;
+}
+console.log('Resultat de la requête:',rows);
+res.json(rows)
 })
-
+})
