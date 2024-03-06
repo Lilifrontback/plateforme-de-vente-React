704b1cac7936233b8d253af4cda1f3eae748a300
Loïck Marin <marin.loick@gmail.com>
2024-03-06 14:41:25 +0100

    page d'accueil Loick

--- front/src/Home.jsx
+++ front/src/Home.jsx
@@ -1,36 +1,65 @@
-import { useState } from "react";
+import React from 'react';
+import './Home.css'; 
+
+const meuble = [
+    {
+        id: 1,
+        nom: "Canapé",
+        prix: 149.99,
+        image: "../src/assets/images/canape_gris.jpg",
+    },
+    {
+        id: 2,
+        nom: "Armoire",
+        prix: 119.99,
+        image: "../src/assets/images/armoire.jpg",
+    },
+    {
+        id: 3,
+        nom: "Commode",
+        prix: 69.99,
+        image: "../src/assets/images/commode.jpg",
+    },
+    {
+        id: 4,
+        nom: "Bibliothèque",
+        prix: 99.99,
+        image: "../src/assets/images/biblio.jpg",
+    },
+    {
+        id: 5,
+        nom: "Canapé",
+        prix: 179.99,
+        image: "../src/assets/images/canape_panacota.jpg",
+    },
+    {
+        id: 6,
+        nom: "Chaises",
+        prix: 39.99,
+        image: "../src/assets/images/chaises.jpg",
+    },
+
+];
 
 function HomePage() {
-
-
-    // state
-    const meubles = [
-        { id: 1, nom: "Canapé", prix: 150 },
-        { id: 2, nom: "Biblio", prix: 100 },
-        { id: 3, nom: "Chaises", prix: 30 },
-    ];
-
-    // comportement
-
-
-    // affichage
-
     return (
-        <div className="App">
-            <h1>Les Chaises Musicales</h1>
-        
-            <div className="meubles">
-                {meubles.map(meuble => (
-                <div key = {meuble.id}>
-                    <h2>{meuble.nom}</h2>
-                    <p>{meuble.prix}€</p>
-                </div>
-                
-            ))}
-        
+        <div className="home-page">
+            <div className="grid">
+                {meuble.map(meuble => (
+                    <div className="container" key={meuble.id}>
+                        <div className="image">
+                            <img src={meuble.image} alt={meuble.nom} />
+                        </div>
+                        <div className="details">
+                            <h2><a>{meuble.nom}</a></h2>
+                            <p>{meuble.prix}€</p>
+                            <button>Ajouter au panier</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-        </div>
-    )
+    );
 }
 
 export default HomePage;
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 import Home from './Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <ChakraProvider>
+      <Home />
+    </ChakraProvider>
   </React.StrictMode>,
 )
