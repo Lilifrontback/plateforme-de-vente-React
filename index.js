b5c9eaf0c0c3bca63c65e36fe6464795c86ea1f1
Jean-Clément <jeanclement@audusseau.fr>
2024-03-07 10:43:05 +0100

    modification de l’accès à la base de donnée et création du fichier .json

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const mysql = require('mysql') //
-const database = mysql.createConnection({
-  host: 'mysql-seguin.alwaysdata.net',
-  user: 'seguin_admin',
-  password: 'Admin1234!',
-  database: 'seguin_chaisemusicale',
-})
+const express = require("express");
+const app = express();
+const port = 3000;
+const databaseCall = require("./connexionDB.json");
+const mysql = require("mysql");
+const database = mysql.createConnection(databaseCall);
 
 database.connect();
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/meubles', function (req, res) {
-database.query("SELECT * FROM Meubles", (err, rows, fields) => {
-  if (err) {
-    console.log("erreur dans la requête",err)
-    res.status(500).send('erreur interne du serveur');
-    return;
-}
-console.log('Resultat de la requête:',rows);
-res.json(rows)
-})
-})
+app.get("/meubles", function (req, res) {
+  database.query("SELECT * FROM Meubles", (err, rows, fields) => {
+    if (err) {
+      console.log("erreur dans la requête", err);
+      res.status(500).send("erreur interne du serveur");
+      return;
+    }
+    console.log("Resultat de la requête:", rows);
+    res.json(rows);
+  });
+});
