c20127e02d1d53410a35511015e6c57a06e6c122
violaine-drt <viodarrent@gmail.com>
2024-03-07 12:26:05 +0100

    added routes GET by ID, DELETE by ID

--- index.js
+++ index.js
@@ -26,3 +26,36 @@ app.get("/meubles", function (req, res) {
     res.json(rows);
   });
 });
+
+app.get("/meubles/:id",  (req, res) => {
+  const id = parseInt(req.params.id)
+  console.log("ID Récupéré: ",id)
+  database.query("SELECT * FROM Meubles WHERE id = ?",[id],(err, rows, fields) => {
+    if (err) {
+      console.log("Le meuble n'a pas été trouvé",err.message);
+      res.status(500).send("erreur interne du serveur");
+      return;
+    }
+    console.log("Resultat de la requête:", rows);
+    res.json(rows);
+ 
+  });
+})
+
+app.delete("/meubles/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+  console.log("ID Récupéré : ", id);
+  database.query(
+    "DELETE FROM Meubles WHERE id = ?",
+    [id],
+    (err, rows, fields) => {
+      if (err) {
+        console.log("erreur dans la requête", err);
+        res.status(500).send("erreur interne du serveur");
+        return;
+      }
+      console.log("Resultat de la requête:", rows);
+      res.json(rows);
+    }
+  );
+});
