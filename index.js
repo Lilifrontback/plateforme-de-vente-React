8d7c0c2a7763ed0f75bdea73eb48a7b71b2d6360
Gaëtane SEGUIN <seguin.gaetane@gmail.com>
2024-03-07 16:22:09 +0100

    ajout de la route PUT

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+//On récupère les paramètres de connection de la BDD
 const databaseCall = require("./connexionDB.json");
-const mysql = require("mysql");
-const database = mysql.createConnection(databaseCall);
+const mysql = require("mysql"); //faire un npm install mysql dans le dossier du projet
+//Appel de la base de donnée
+const database = mysql.createConnection(databaseCall); 
 app.use(express.json());
-
+//connection à la base de donnée
 database.connect();
 
+//Permet de lancer le serveur
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+//Route de test pour vérifier que le serveur est bien lancé 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -30,7 +34,7 @@ app.post("/meubles",(req,res) => {
   VALUES
   (
       ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? 
-  )`; // Les ? seront remplacés par les valeurs à ajouter
+  )`; // Les ? seront remplacés par les valeurs à ajouter Attention à bien mettre les variables à ajouter dans le même ordre que la liste ci-dessus
   //envoie de la requête
   database.query(addMeubles,[meubleAjoute.nom,meubleAjoute.categorie_id,meubleAjoute.descriptif,meubleAjoute.prix,meubleAjoute.dimension, meubleAjoute.vendeur_id,meubleAjoute.acheteur_id,meubleAjoute.matiere_id,meubleAjoute.photo,meubleAjoute.couleur_id,meubleAjoute.stock])
   res.status(201).json({
@@ -52,6 +56,34 @@ app.get("/meubles", function (req, res) {
   });
 });
 
+
+//modification des paramètres d'un meuble. /!\ seulement fait pour couleur_id mais possible de le faire pour tous
+app.put("/meubles/:id", (req, res) =>{
+  const recup = (req.body) // Récupération des données du form en format Json
+  const id = parseInt(req.params.id) //Récupération de l'id via la route
+  const couleurId = recup.couleur_id
+  console.log("couleurId recupéré : ",couleurId)
+  console.log("ID Récupéré: ",id)
+  let paramQuery=[couleurId,id] //Bien mettre les paramètres dans ce tableau dans l'ordre des points d'interrogation de la requête
+  database.query("UPDATE Meubles SET couleur_id = ? WHERE id = ?",paramQuery,(err, result) => {
+    if(err){
+      res.json({
+        status:400,
+        message:err
+      })
+    }
+
+    else{
+      res.json({
+        status:200,
+        message:result
+      })
+    }
+  });
+})
+
+
+//Route pour récupérer un meuble en fonction de son id
 app.get("/meubles/:id",  (req, res) => {
   const id = parseInt(req.params.id)
   console.log("ID Récupéré: ",id)
@@ -67,6 +99,8 @@ app.get("/meubles/:id",  (req, res) => {
   });
 })
 
+//Route pour supprimer un meuble en fonction de son id (afin de ne PAS supprimer toute la table ;) )
+//Pour faire le test on a créé une chaise test dans la table afin de ne pas supprimer les données créées par Jean-Clément
 app.delete("/meubles/:id", function (req, res) {
   const id = parseInt(req.params.id);
   console.log("ID Récupéré : ", id);
