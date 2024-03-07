c0bbd5dd91840ac4a41a64cdcd1bdf5d043f2672
Loïck Marin <marin.loick@gmail.com>
2024-03-07 16:15:15 +0100

    commentaires + explications routes

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,7 +1,10 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
+
+// Installation npm install react-router-dom
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import Home from './Home.jsx'
 import Product from './Product.jsx'
 
@@ -11,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* Path = '/' -> page par défaut */}
           <Route path="/product/:id" element={<Product />} />
+          {/* Path = '/"exemple"' -> permet d'accder à la page correspondante voir <Link> page Home */}
         </Routes>
       </Router>
     </ChakraProvider>
