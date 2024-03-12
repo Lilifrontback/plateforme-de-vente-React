3d4fb68ba7c0f4b0e5d27e33b9ed2d020ad1a03f
Gaëtane SEGUIN <seguin.gaetane@gmail.com>
2024-03-12 16:24:03 +0100

    commencement de l'ajout d'un meuble dans le front

--- front/src/composants/admin.jsx
+++ front/src/composants/admin.jsx
@@ -14,23 +14,33 @@ import {
   TableContainer,
   Button, 
   Stack,
-  Input
+  Input,
 } from '@chakra-ui/react';
 
 //Import données meubles
 let meubles= await fetchMeublesEnStock()
     .catch(error => console.error('Error:', error))
-
+    
+    
 function Admin() {
-  
-  const categories = ['Catégorie 1', 'Catégorie 2', 'Catégorie 3'];
+  const [nomMeuble, setName] = React.useState('');
+  const [newDescription, setValue] = React.useState('');
+  const [newPhoto, setPhoto] = React.useState('');
+  const [newPrix, setPrix] = React.useState('');
+  const [newStock, setStock] = React.useState('');
+  const handleChange = (event) => setName(event.target.value)
+  const DescriptionChange = (event) => setValue(event.target.value)
+  const photoChange = (event) => setPhoto(event.target.value)
+  const prixChange = (event) => setPrix(event.target.value)
+  const stockChange = (event) => setStock(event.target.value)
+  const categories = ['Canape', 'Catégorie 2', 'Catégorie 3'];
   const matières = ['mat 1', 'mat 2', 'mat 3'];
   console.log(meubles)
   typeof(meubles)
 
      return (
 <TableContainer>
-  <Table _css={{"table-layout":"fixed","width": '5%'}} variant='striped'>
+  <Table variant='striped'>
     <TableCaption>inventaire</TableCaption>
     {/* nom des catégories */}
     <Thead>
@@ -53,29 +63,35 @@ function Admin() {
     <Tbody>
       <Tr>
         {/* insertion champ libre */}
-      <Td><Input placeholder="Entrez un nom" /></Td>
-      <Td><Input placeholder="ajouter une photo" /></Td>
-      {/* menu déroulant */}
-      <Td><select><option value="">catégorie</option>
-      {categories.map((categorie, index) => (
-      <option key={index} value={categorie}>{categorie}</option>
-       ))}
-      </select></Td>
-      <Td><Input placeholder="saisir une description" /></Td>
-         <Td><select><option value="">matière</option>
-      {matières.map((matières, index) => (
-      <option key={index} value={matières}>{matières}</option>
-      ))}
-       </select></Td>
-      <Td><Input placeholder="saisir/choisir une couleur" /></Td>
-      <Td><Input placeholder="en stock" /></Td>
-      <Td><Input placeholder="acheteur" /></Td>
-      <Td><Input placeholder="saisir un prix" /></Td>
-      {/* Bouton */}
-      <Td><Button colorScheme='teal' variant='ghost'> 
-      {/* onClick={handleAddItem} à rajouter au bouton*/}
-    ajouter</Button></Td>  
-      </Tr>
+      <Td><Input placeholder="Entrez un nom" value={nomMeuble}
+        onChange={handleChange}/> 
+        {console.log(nomMeuble)}  </Td>
+      <Td><Input placeholder="ajouter une photo" value={newPhoto}
+        onChange={photoChange}/> </Td>
+        {/* menu déroulant */}
+        <Td><select><option value="">catégorie</option>
+        {categories.map((categorie, index) => (
+        <option key={index} value={categorie}>{categorie}</option>
+        ))}
+        </select></Td>
+        <Td><Input placeholder="saisir une description" value={newDescription}
+        onChange={DescriptionChange}/> {console.log(newDescription)} </Td>
+          <Td><select><option value="">matière</option>
+        {matières.map((matières, index) => (
+        <option key={index} value={matières}>{matières}</option>
+        ))}
+        </select></Td>
+        <Td><Input placeholder="saisir/choisir une couleur" /></Td>
+        <Td><Input placeholder="en stock" value={newStock}
+        onChange={stockChange}/></Td>
+        <Td><Input placeholder="acheteur" /></Td>
+        <Td><Input placeholder="saisir un prix" value={newPrix}
+        onChange={prixChange}/></Td>
+        {/* Bouton */}
+        <Td><Button colorScheme='teal' variant='ghost'  > 
+        {/* onClick={handleAddItem} */}
+            ajouter</Button></Td>  
+        </Tr>
           {meubles.map((meuble,index) => ( //boucle qui parcourt l'objet
             <Tr key={index}>
               <Td>{meuble.nom}</Td>          
@@ -88,8 +104,8 @@ function Admin() {
               <Td>{meuble.acheteur_id}</Td>
               <Td>{meuble.prix}</Td>
              
+              <Td> XXXXX </Td>
               
-              <Td> <Button colorScheme='teal' variant='ghost'>ajouter</Button></Td>
               <Td> <Button colorScheme='teal' variant='ghost'>modifier</Button></Td>
               <Td> <Button colorScheme='teal' variant='ghost'>supprimer</Button></Td>
              
