770d74c5032742f372d522b603608978584d3a7a
violaine-drt <viodarrent@gmail.com>
2024-03-12 16:43:12 +0100

    FOnctionnalités de filtrage en cours

--- front/src/Home.jsx
+++ front/src/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import SearchBar from "../src/composants/SearchBar.jsx";
 import Filtres from "../src/composants/Filters.jsx";
 import { Link } from "react-router-dom";
@@ -9,7 +9,6 @@ import { SimpleGrid } from "@chakra-ui/react";
 
 //Import données 
 import {fetchMeubles} from "./services/apiService.jsx";
-// import { fetchMeublesFiltres } from "./services/apiService.jsx";
 
 // On export la constante pour la récupérer dans d'autres pages
 export const meubles = await fetchMeubles().catch((error) =>
@@ -17,18 +16,28 @@ export const meubles = await fetchMeubles().catch((error) =>
 );
 
 function Home() {
-  const filtreParCategorie = ['Chaise', 'Table', 'Lit'];
-  const selectionFiltre = (filtreChoisi) => {
-    // Filtrer les meubles en fonction du filtre sélectionné
-    const meublesFiltrés = meubles.filter((meuble) => meuble.categorie === filtreChoisi);
-    setFilteredMeubles(meublesFiltrés);
-    console.log(`Filtre sélctionné: ${filtreChoisi}`);
+  const filtreParCouleur = ['Chêne clair','Vert','bleu nuit', 'Beige', 'Rouge', 'Doré', 'Chêne foncé', 'Gris', 'Acajou', 'Noir']
+  const filtreParCategorie = ['Chaise', 'Table', 'Lit', 'Canapé', 'Fauteuil', 'Cathèdre', 'Vaisselier', 'Armoire', 'Dressoir', 'Cabinet', 'Commode'];
+  const filtreParMatiere = ['Bois', 'Cuir', 'Velour', 'Verre', 'Plastique', 'Pierre', 'Rotin', 'Tissu'];
+
+//On garde en mémoire
+  const [filteredMeubles, setFilteredMeubles] = useState(meubles);
+
+// Filtrer les meubles en fonction du filtre sélectionné, et d'une propriété qu'on veut chercher pour comparer au filtre (on appelle le paramètre filtreChoisi)
+  function selectionFiltre (filtreChoisi,propriete) {
+    const meublesFiltrés = meubles.filter(function (meuble) {
+      return meuble[propriete] === filtreChoisi;
+  });
+  //On change la valeur de FilterMeubles dans le useState
+  setFilteredMeubles(meublesFiltrés);
+    console.log(`Filtre sélectionné: ${filtreChoisi}`);
   };
   return (
     <Stack spacing={8} align="center" mt={8}>
       <div>
-      <Filtres filters={filtreParCategorie} onSelectFilter={selectionFiltre} />
-      {/* Render your data based on the selected filter */}
+      <Filtres filters={filtreParCategorie} onSelectFilter={(filtreChoisi) => selectionFiltre(filtreChoisi, "categorie")} />
+      <Filtres filters={filtreParCouleur} onSelectFilter={(filtreChoisi) => selectionFiltre(filtreChoisi, "couleur")} />
+      <Filtres filters={filtreParMatiere} onSelectFilter={(filtreChoisi) => selectionFiltre(filtreChoisi, "matiere")} />
     </div>
     <SearchBar />
       <SimpleGrid
@@ -36,7 +45,7 @@ function Home() {
         templateColumns="repeat(3,1fr)"
         justifyItems="center"
       >
-        {meubles.map((meuble) => (
+        {filteredMeubles.map((meuble) => (
           <Card key={meuble.id} maxW="xs">
             <CardBody>
               <Image src={meuble.photo} alt={meuble.nom} borderRadius="lg" />
--- front/src/composants/Filters.jsx
+++ front/src/composants/Filters.jsx
@@ -2,25 +2,25 @@ import React, { useState } from 'react';
 import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
 import { Button } from '@chakra-ui/react';
 
-// Filtres prend deux propriétés en param : liste de filtre, et un callback qd le filtre est sélectionné
-const Filtres = ({ filters, onSelectFilter }) => {
+//Filtres prend deux propriétés en param : liste de filtre, et un callback qd le filtre est sélectionné
+function Filtres ({ filters, onSelectFilter }) {
 
-//stockage avec useSate du filtre sélectionné
-  const [selectedFilter, setSelectedFilter] = useState(null);
+//On garde en mémoire avec useSate le filtre sélectionné 
+const [selectedFilter, setSelectedFilter] = useState(null);
 
-//Fonction qui prend en paramètre un filtre et met à jour le useState "slectedFilter"
-//Appel de la fonction "onSelectFilter" sur le filtre choisi
-  const gererFiltreChoisi = (fitreChoisi) => {
-    setSelectedFilter(fitreChoisi);
-    onSelectFilter(fitreChoisi);
-  };
-
+//On crée une fonction gérerFiltreChoisi qui appelle onSelectFilter sur le filtre choisi
+function gererFiltreChoisi (fitreChoisi) {
+  setSelectedFilter(fitreChoisi);
+  onSelectFilter(fitreChoisi);
+};
+//On retourne le bouton
   return (
     <Menu>
       <MenuButton as={Button}>Filtrer: {selectedFilter || 'Choisir le filtre'}</MenuButton>
       <MenuList>
+        {/* On va appeller sur le click du filtre, la fonction qui garde en mémoire le filtre choisi */}
         {filters.map((filter) => (
-          <MenuItem key={filter} onClick={() => handleSelectedFilters(filter)}>
+          <MenuItem key={filter} onClick={() => gererFiltreChoisi(filter)}>
             {filter}
           </MenuItem>
         ))}
