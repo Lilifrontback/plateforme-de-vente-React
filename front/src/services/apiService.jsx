7803b188f33ad0eba2095bd35f32a45c2666a7f4
Jean-Clément <jeanclement@audusseau.fr>
2024-03-12 11:31:08 +0100

    injection et affichage des données sur la page Home du site

--- front/src/services/apiService.jsx
+++ front/src/services/apiService.jsx
@@ -1,9 +1,8 @@
-import React from "react"
-const apiUrl = 'http://localhost:3000/'
+import React from "react";
+const apiUrl = "http://localhost:3000/";
 
 function fetchMeubles() {
-return fetch(`${apiUrl}meubles`)
-.then(response => response.json())
+  return fetch(`${apiUrl}meubles`).then((response) => response.json());
 }
 
-export default fetchMeubles
+export default fetchMeubles;
